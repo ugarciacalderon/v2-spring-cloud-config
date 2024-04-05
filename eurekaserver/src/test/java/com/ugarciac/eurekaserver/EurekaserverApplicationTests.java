3c26e0646d4b9a729b51724f3b466c73c37a9bee
Ulises Garcia <ugarciacalderon@gmail.com>
2024-04-04 18:02:48 -0600

    added netflix-eureka-server config

---
+++ eurekaserver/src/test/java/com/ugarciac/eurekaserver/EurekaserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ugarciac.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
